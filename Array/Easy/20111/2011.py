52f20c3a41a0fc42f76bdb568a697f3d3737cc6f
StefankinaO <60422917+StefankinaOlya@users.noreply.github.com>
2022-05-29 12:43:56 -0200

    Delete Array/Easy/20111 directory

--- Array/Easy/20111/2011.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def finalValueAfterOperations(self, operations: List[str]) -> int:
-        z = 0
-        for i in range(len(operations)):
-            if operations[i].find("--") != -1:
-                z -= 1
-            else:
-                z += 1
-        return z
